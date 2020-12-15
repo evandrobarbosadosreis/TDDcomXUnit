aa2f120de5cc59af47685f97b740e105056ac72c
Evandro B. dos Reis <evandro.reis@wlesoft.com.br>
2020-12-15 09:57:59 -0300

    Adicionado teste para salvar curso com o mesmo nome

--- CursoOnline.Dominio.Test/Cursos/ArmazenadorDeCursoTest.cs
+++ CursoOnline.Dominio.Test/Cursos/ArmazenadorDeCursoTest.cs
@@ -1,6 +1,9 @@
+using System;
 using Bogus;
 using CursoOnline.Dominio.Enums;
 using CursoOnline.Dominio.Models;
+using CursoOnline.Dominio.Test.Builders;
+using CursoOnline.Dominio.Test.Extensions;
 using Moq;
 using Xunit;
 
@@ -8,13 +11,15 @@ namespace CursoOnline.Dominio.Test.Cursos
 {
     public class ArmazenadorDeCursoTest
     {
+        private readonly CursoDTO _cursoDTO;
+        private readonly Mock<ICursoRepository> _repository;
+        private readonly ArmazenadorDeCurso _armazenador;
 
-        [Fact]
-        public void DeveAdicionarNovoCurso()
+        public ArmazenadorDeCursoTest()
         {
-            //Given
             var fake = new Faker();
-            var cursoDTO = new CursoDTO
+            
+            _cursoDTO = new CursoDTO
             {
                 Nome         = fake.Random.Word(),
                 Descricao    = fake.Lorem.Paragraph(),
@@ -22,20 +27,44 @@ namespace CursoOnline.Dominio.Test.Cursos
                 CargaHoraria = fake.Random.Int(1, 180),
                 PublicoAlvo  = fake.Random.Enum<EPublicoAlvo>()
             };
-            var repository  = new Mock<ICursoRepository>();
-            var armazenador = new ArmazenadorDeCurso(repository.Object);
-            
+
+            _repository  = new Mock<ICursoRepository>();
+            _armazenador = new ArmazenadorDeCurso(_repository.Object);            
+        }
+
+
+        [Fact]
+        public void DeveAdicionarNovoCurso()
+        {            
             //When
-            armazenador.Armazenar(cursoDTO);
+            _armazenador.Armazenar(_cursoDTO);
             
             //Then
-            repository.Verify(r => r.Adicionar(It.Is<Curso>(curso => 
-                curso.Nome == cursoDTO.Nome && 
-                curso.Descricao == cursoDTO.Descricao && 
-                curso.Valor == cursoDTO.Valor &&
-                curso.CargaHoraria == cursoDTO.CargaHoraria &&
-                curso.PublicoAlvo == cursoDTO.PublicoAlvo
+            _repository.Verify(r => r.Adicionar(It.Is<Curso>(curso => 
+                curso.Nome == _cursoDTO.Nome && 
+                curso.Descricao == _cursoDTO.Descricao && 
+                curso.Valor == _cursoDTO.Valor &&
+                curso.CargaHoraria == _cursoDTO.CargaHoraria &&
+                curso.PublicoAlvo == _cursoDTO.PublicoAlvo
             ))); 
+        }
+
+        [Fact(DisplayName = "NaoDeveSalvarCursoComMesmoNome")]
+        public void NaoDeveSalvarCursoComMesmoNome()
+        {
+            //Given
+            var nomeCursoRepetido = _cursoDTO.Nome;
+            var cursoJaCadastrado = CursoBuilder
+                .Novo()
+                .ComNome(nomeCursoRepetido)
+                .Build();
+            _repository.Setup(repo => repo.BuscarPorNome(nomeCursoRepetido)).Returns(cursoJaCadastrado);
+
+            //When
+            Action action = () => _armazenador.Armazenar(_cursoDTO);
+            
+            //Then
+            Assert.Throws<ArgumentException>(action).WithMessage("Já existe um curso cadastrado com esse nome");
         }
     }
 
@@ -51,6 +80,7 @@ namespace CursoOnline.Dominio.Test.Cursos
     public interface ICursoRepository
     {
         void Adicionar(Curso curso);
+        Curso BuscarPorNome(string nome);
     }
 
     public class ArmazenadorDeCurso
@@ -64,6 +94,13 @@ namespace CursoOnline.Dominio.Test.Cursos
 
         public void Armazenar(CursoDTO dto)
         {
+            var cursoJaCadastrado = _repository.BuscarPorNome(dto.Nome);
+
+            if (cursoJaCadastrado != null)
+            {
+                throw new ArgumentException("Já existe um curso cadastrado com esse nome");
+            }
+
             var curso = new Curso(
                 dto.Nome, 
                 dto.Descricao,
