39f00968820216e2bf429c67128e9567104897d2
Evandro B. dos Reis <evandro.reis@wlesoft.com.br>
2020-12-15 11:03:46 -0300

    Adicionado projeto de webapi e projeto de config. dos serviços de infra

---
+++ CursoOnline.Infra.Config/InfraConfigStartup.cs
@@ -0,0 +1,19 @@
+﻿using CursoOnline.Dominio.Interfaces;
+using CursoOnline.Dominio.Services;
+using CursoOnline.Infra.Context;
+using CursoOnline.Infra.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CursoOnline.Infra.Config
+{
+    public static class InfraConfigStartup
+    {
+        public static void AddInfraServices(this IServiceCollection services)
+        {
+            services.AddDbContext<InMemoryContext>(options => options.UseInMemoryDatabase("database"));
+            services.AddScoped<ICursoRepository, CursoRepository>();
+            services.AddScoped<IAdicionarCursoCommand, AdicionarCursoCommand>();
+        }
+    }
+}
---
+++ CursoOnline.Webapi/Startup.cs
@@ -0,0 +1,47 @@
+using CursoOnline.Infra.Config;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace CursoOnline.Webapi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddInfraServices();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CursoOnline.Webapi", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CursoOnline.Webapi v1"));
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
